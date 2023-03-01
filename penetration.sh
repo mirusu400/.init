4c626590516543f9ef2e9071fe749a2ef3a4fc3c
mirusu400 <mirusu400@naver.com>
2023-03-01 22:45:19 +0900

    Update penetration.sh

--- penetration.sh
+++ penetration.sh
@@ -9,6 +9,7 @@ sudo apt install -y feroxbuster
 
 # Install gobuster
 go install github.com/OJ/gobuster/v3@latest
+mv ./go/bin/gobuster /usr/local/bin
 
 # Update pip
 python3 -m pip install --upgrade pip
