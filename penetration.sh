456c195c2da2908f8add5374c33c99510e5c6e22
mirusu400 <mirusu400@naver.com>
2023-03-01 22:42:32 +0900

    Create penetration.sh

---
+++ penetration.sh
@@ -0,0 +1,18 @@
+
+sudo apt-get update
+
+# Install golang
+sudo apt-get install golang-go -y
+
+# Install feroxbuster
+sudo apt install -y feroxbuster
+
+# Install gobuster
+go install github.com/OJ/gobuster/v3@latest
+
+# Update pip
+python3 -m pip install --upgrade pip
+
+# Update pwncat-cs
+pip3 install pwncat-cs
+
