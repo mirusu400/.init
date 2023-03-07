f7c51b9e1f137805b76659ad629d9a867177f05b
mirusu400 <mirusu400@naver.com>
2023-03-07 01:07:54 -0500

    Update penetration.sh

--- penetration.sh
+++ penetration.sh
@@ -6,7 +6,7 @@ echo "deb-src http://ftp.harukasan.org/kali kali-rolling main non-free contrib" 
 echo "deb http://ftp.harukasan.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
 
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean
-
+sudo apt update
 sudo apt-get install fcitx-hangul -y
 sudo apt-get install fcitx-lib* -y
 sudo apt-get install fonts-nanum* -y
