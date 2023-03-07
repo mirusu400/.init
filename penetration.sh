2591e8776021e8c5be8fd67cbebfdb2118bf23cc
mirusu400 <mirusu400@naver.com>
2023-03-07 01:04:03 -0500

    Update penetration.sh

--- penetration.sh
+++ penetration.sh
@@ -1,5 +1,15 @@
 
-sudo apt-get update
+
+
+# Korean support
+echo "deb-src http://ftp.harukasan.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
+echo "deb http://ftp.harukasan.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
+
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean
+
+sudo apt-get install fcitx-hangul -y
+sudo apt-get install fcitx-lib* -y
+sudo apt-get install fonts-nanum* -y
 
 # Install golang
 sudo apt-get install golang-go -y
