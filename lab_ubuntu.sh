d383749e87971f9dac6a3eadd374e1ddbc5b6b59
mirusu400 <mirusu400@naver.com>
2024-03-28 17:08:10 -0400

    update my lab ubuntu

---
+++ lab_ubuntu.sh
@@ -0,0 +1,10 @@
+sudo apt-get -y update
+sudo snap install code
+sudo snap install slack
+sudo snap install discord
+sudo apt-get -y install gnome-tweak-tool
+sudo apt-get -y install neovim
+sudo apt-get -y install ibus-hangul
+sudo apt-get -y install git
+git config --global user.name "mirusu400"
+git config --global user.email "mirusu400@naver.com"
