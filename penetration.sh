a821251e1f7ab74b08b6b52d459e8e3548137e9c
mirusu400 <mirusu400@naver.com>
2024-06-01 11:32:42 +0900

    Update penetration.sh

--- penetration.sh
+++ penetration.sh
@@ -44,7 +44,8 @@ curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 sudo python2 get-pip.py
 sudo python2 -m pip install -U setuptools wheel
 
-python2 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
+python2 -m pip install -U  yara pycrypto pillow openpyxl ujson pytz ipython capstone
+pip2 install distorm3==3.3.4
 sudo python2 -m pip install yara
 sudo ln -s /usr/local/lib/python2.7/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
 python2 -m pip install -U git+https://github.com/volatilityfoundation/volatility.git
