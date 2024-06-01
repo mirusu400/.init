9454cfb546b54f43e1bfb06bf52fe4c3245020e8
mirusu400 <mirusu400@naver.com>
2024-06-01 11:31:21 +0900

    Update penetration.sh

--- penetration.sh
+++ penetration.sh
@@ -38,6 +38,27 @@ cd RustScan
 cargo build --release
 sudo ln -s ~/RustScan/target/release/rustscan /usr/bin/rustscan
 
+# Install volatility2
+sudo apt install -y python2 python2.7-dev libpython2-dev
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+sudo python2 -m pip install -U setuptools wheel
+
+python2 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
+sudo python2 -m pip install yara
+sudo ln -s /usr/local/lib/python2.7/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
+python2 -m pip install -U git+https://github.com/volatilityfoundation/volatility.git
+
+
+
+# install volatility3
+sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
+
+python3 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
+python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
+
+echo "export PATH=/home/`whoami`/.local/bin:$PATH\n" >> ~/.bashrc
+. ~/.bashrc
 
 
 
