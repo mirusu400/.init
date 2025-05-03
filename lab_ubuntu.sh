de703894d8359a0d5b65cf77ca31f4667549e0bb
mirusu400 <mirusu400@naver.com>
2025-05-03 21:01:25 +0900

    update keyboard setup

--- lab_ubuntu.sh
+++ lab_ubuntu.sh
@@ -1,6 +1,10 @@
 sudo apt-get -y update
 sudo snap install slack
 sudo snap install discord
+sudo apt install -y build-essential libssl-dev zlib1g-dev \
+  libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev \
+  libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev \
+  wget curl git
 sudo apt-get -y install gnome-tweak-tool
 sudo apt-get -y install neovim
 sudo apt-get -y install ibus-hangul
@@ -25,8 +29,10 @@ sudo apt install ./google-chrome*.deb
 sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
 && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
+&& sudo apt update 
 && sudo apt install gh -y
 
+echo "input method -> Korean - Hangul"
+ibus-setup
 gh auth login
 
