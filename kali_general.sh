b86181f667b03bb4c5921d198640121ab88eb6c7
mirusu400 <mirusu400@naver.com>
2026-02-27 15:50:54 +0900

    Update Rust installation command to use -y flag
    Automatically confirm Rust installation during setup.

--- kali_general.sh
+++ kali_general.sh
@@ -55,7 +55,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Install rust
 cd ~
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
 git clone https://github.com/RustScan/RustScan
